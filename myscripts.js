5ea56cd23e3e57aeddfe1c72c8ef88495aedc5ce
Vlad <linnikv10@gmail.com>
2022-07-20 21:09:53 +0300

    first push

---
+++ myscripts.js
@@ -0,0 +1,50 @@
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+function downloadURI(uri, name) {
+  var link = document.createElement("a");
+  link.download = name;
+  link.href = uri;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+  delete link;
+}
+let timer = 600;
+var timer_elem = document.getElementById('p');
+timerIsWorking = true
+const IntervalTimer = setInterval(myTimer,1000);
+function myTimer(){
+	if (timerIsWorking){
+		let minutes = Math.floor(timer/60);
+		let seconds = timer % 60;
+		minutes = minutes < 10 ? '0' + minutes : minutes;
+		seconds = seconds < 10 ? '0' + seconds : seconds;
+		timer_elem.innerHTML = `${minutes}:${seconds}`;
+		timer--;
+		if (timer == -1) {
+			timer = 600;
+		}
+	}
+}
+document.getElementsByClassName("button")[0].addEventListener('click',function(){
+	console.log("start")
+	timerIsWorking = true;
+});
+document.getElementsByClassName("button")[1].addEventListener('click',function(){
+	console.log("stop")
+	timerIsWorking = false;
+});
+document.getElementsByClassName("button")[2].addEventListener('click',function(){
+	console.log("skip");
+	timerIsWorking = true;
+	timer = 600;
+	//let link = JSON.parse(httpGet("https://randomfox.ca/floof/"))["image"];
+	//console.log(link);
+	//downloadURI(link , "asd.jpg"); - not working
+	xhr.send(data);
+});
